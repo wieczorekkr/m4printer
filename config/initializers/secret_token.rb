fe6bbedb9645e490fcca8ec65f239ee6e8d60973
Krzysztof <maverik0@wp.pl>
2014-03-10 21:37:32 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+M4printer::Application.config.secret_key_base = '2b596048492bad4f50b1f540e51a94c19cfa647dd97cdbacbc7db1b7b756a602bba1689bcde669df406e899cc32f4cc0e44531cefdcd750bbfcd6b1986d8e6ca'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+M4printer::Application.config.session_store :cookie_store, key: '_m4printer_session'
